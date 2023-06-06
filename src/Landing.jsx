84c996e3190a9e84d0b749d032404d3ef543bbab
afu5 <strylcast@gmail.com>
2023-06-06 02:37:11 -0700

    changed backend link

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -12,7 +12,7 @@ export default function Landing() {
   const {experience, setExperience} = useContext(ExperienceContext)
 
   useEffect(() => {
-    const newSocket = io('http://localhost:8080', {
+    const newSocket = io('https://hexwar-backend.herokuapp.com', {
       transports: ['websocket']
     });
     setSocket(newSocket)
